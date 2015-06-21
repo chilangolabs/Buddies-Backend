bd10d8dcf54a904fa3c9c8b9153039a68e084dbf
Rodrigo Moreno <moreno.rdr@gmail.com>
2015-06-21 03:25:51 -0500

    ^ Prueba con Twilio

--- controllers/config/sms.js
+++ controllers/config/sms.js
@@ -1,9 +1,12 @@
 'use strict';
 
+var mdwIsAuthenticated = require('../../lib/passport').isAuthenticated;
+var twilio = require('../../lib/twilio');
+
 module.exports = function(router) {
 
-  router.get('/', function(req, res) {
-    res.render('index');
+  router.get('/', mdwIsAuthenticated, function(req, res) {
+    twilio.sendCode(req.user.phoneNumber);
   });
 
 };
