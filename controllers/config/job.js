0d87197ea93447c1913a81360c6ca6ee27fc39cf
Christian Ceciliano <cristiancehi@gmail.com>
2015-06-21 11:12:12 -0500

    ^ Mongoose queen

--- controllers/config/job.js
+++ controllers/config/job.js
@@ -9,7 +9,7 @@ module.exports = function(router) {
   router.post('/addTalents', mdwIsAuthenticated, function(req, res, next) {
     var talents = req.body.talents || req.query.talents;
     if (!talents) {return res.sendStatus(400);}
-    req.user.update({$push: {talents: {$each: talents}}}, function(err, user) {
+    req.user.update({'$push': {'talents': {'$each': talents}}}, function(err, user) {
       if (err) {return res.sendStatus(500);}
       res.status(200).json({talents: user.talents});
     });
