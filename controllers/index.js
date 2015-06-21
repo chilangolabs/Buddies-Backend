0a2ce4d866a4befee5f4ff50f432757cf4dc40b3
Christian Ceciliano <cristiancehi@gmail.com>
2015-06-21 01:24:05 -0500

    + Fb profile test

--- controllers/index.js
+++ controllers/index.js
@@ -7,7 +7,15 @@ module.exports = function(router) {
   var model = new IndexModel();
 
   router.get('/', function(req, res) {
-    res.render('index');
+    res.format({
+      json: function() {
+        res.json(JSON.stringify(req.user));
+      },
+      default: function() {
+        res.locals.user = req.user;
+        res.render('index');
+      }
+    });
   });
 
 };
