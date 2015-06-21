056d894f87d0f0ace7e761a9edb4d3435212da54
Christian Ceciliano <cristiancehi@gmail.com>
2015-06-21 00:06:03 -0500

    + Facebook authentication

---
+++ controllers/auth/facebook.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var passport = require('passport');
+
+module.exports = function(router) {
+
+  router.get('/',
+    passport.authenticate('facebook',
+      {scope: ['public_profile', 'email', 'user_friends']}));
+  router.get('/callback',
+    passport.authenticate('facebook', {
+      successRedirect: '/',
+      failureRedirect: '/'
+    }
+  ));
+
+};
