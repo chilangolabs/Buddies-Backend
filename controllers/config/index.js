f3a1b35bb2f8f104b2f00de8d9f10d736b1ccc4b
Christian Ceciliano <cristiancehi@gmail.com>
2015-06-21 07:10:07 -0500

    ^ Seting phone

--- controllers/config/index.js
+++ controllers/config/index.js
@@ -1,17 +1,18 @@
 'use strict';
 
 var mdwIsAuthenticated = require('../../lib/passport').isAuthenticated;
+var twilio = require('../../lib/twilio');
 
 module.exports = function(router) {
 
-  router.post('/phone', mdwIsAuthenticated, function(req, res) {
+  router.post('/phone', mdwIsAuthenticated, function(req, res, next) {
     var phone = req.body.phone || req.query.phone;
     if (!phone) {return res.status(400);}
     req.user.phoneNumber = phone;
     req.user.save(function(err, user) {
       if (err) {return res.redirect('/error');}
-      res.redirect('sms/');
+      next();
     });
-  });
+  }, twilio.sendCode);
 
 };
--- controllers/config/sms.js
+++ controllers/config/sms.js
@@ -5,8 +5,8 @@ var twilio = require('../../lib/twilio');
 
 module.exports = function(router) {
 
-  router.get('/', mdwIsAuthenticated, twilio.sendCode);
-  router.get('/validate', mdwIsAuthenticated, function(req, res, next) {
+  router.get('/resend', mdwIsAuthenticated, twilio.sendCode);
+  router.post('/validate', mdwIsAuthenticated, function(req, res, next) {
     var code = req.body.code || req.query.code;
     if (!code) {
       return res.status(400);
